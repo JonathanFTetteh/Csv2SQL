246367c7c5ee6b04b845090750392f2b6c90165b
JonathanFTetteh <tt-jon@gmx.de>
2024-01-12 10:26:35 +0100

    update 12.01.2024 Klassendiagramm

--- src/main/java/de/srh/java/tetteh/Locations.java
+++ src/main/java/de/srh/java/tetteh/Locations.java
@@ -8,14 +8,6 @@ public class Locations {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
     public Locations(String city, String country) {
         this.city = city;
         this.country = country;
